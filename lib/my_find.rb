314752d10d7fccb09445500dd2c1ca5a7a552c71
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-16 00:06:52 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_find(collection) # does not call on #find
   i = 0
   while i < collection.length
-    return collection[i] if yield(collection[i])
+    return collection[i] if yield(collection[i]) #yields the correct element to the block, 
     i = i + 1
   end
 end
