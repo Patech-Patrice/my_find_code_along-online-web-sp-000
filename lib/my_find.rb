4d0fc108dc7b1cba4cc8c90d00d3acec7e7dd8c8
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-16 00:07:07 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_find(collection) # does not call on #find
   i = 0
   while i < collection.length
-    return collection[i] if yield(collection[i]) #yields the correct element to the block, 
+    return collection[i] if yield(collection[i]) #yields the correct element to the block, returns the elements from the array when the block condition is met, 
     i = i + 1
   end
 end
